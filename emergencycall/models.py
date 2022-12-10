09145287164de340512da969bae19fe6d3130f24
irsyadmufid <irsyadm90@gmail.com>
2022-12-11 01:00:48 +0700

    add views

--- emergencycall/models.py
+++ emergencycall/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class EmergencyCallItem(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True )
     hospital_name = models.CharField(max_length=255)
     hospital_number = models.IntegerField()
     hospital_location = models.TextField()
--- tempat_kuliner/models.py
+++ tempat_kuliner/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class tempat_kuliner_Item(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     nama_tempat_kuliner = models.TextField()
     rating_tempat_kuliner = models.CharField(max_length = 10)
     lokasi_tempat_kuliner = models.TextField()
